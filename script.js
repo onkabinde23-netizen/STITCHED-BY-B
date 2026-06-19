6378d711f75f68d9c838630df42695a308b69e0d
Olwethu Nkabinde <o.nkabinde23@gmail.com>
2026-06-19 13:58:56 +0200

    i created a Javascript folder

---
+++ script.js
@@ -0,0 +1,20 @@
+// ==============================
+// STITCHED BY B JAVASCRIPT FILE
+// ==============================
+
+// Welcome Message
+window.onload = function () {
+    alert("Welcome to STITCHED BY B - From My Hands To Your Heart!");
+};
+
+// Contact Form Message
+function submitMessage() {
+    alert("Thank you for contacting STITCHED BY B. We will get back to you soon!");
+}
+
+
+
+// Gallery Message
+function galleryMessage() {
+    alert("Thank you for viewing our handmade creations!");
+}
